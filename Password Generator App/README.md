bfd46335df9784c8c56d5861e87c048bcae2eed5
Gabriel de Alencar <gabrieldealencar.dev@gmail.com>
2024-04-12 23:24:35 -0300

    full project [11]

---
+++ Password Generator App/README.md
@@ -0,0 +1,53 @@
+# Frontend Mentor - Password Generator App
+
+![desktop-preview](https://github.com/gabrielalencs/Frontend-Mentor-Challenges/blob/main/News%20Homepage%20Main/assets/images/print.PNG)
+
+**Esse repositório é destinado as minhas soluções dos desafios da plataforma FrontEnd Mentor**
+
+## Olá, Seja Bem Vindo! 👋
+
+**Obrigado por conferir essa solução para esse desafio!!!** 
+<br>
+Vamos conferir de forma geral esse projeto.
+
+## Sobre o Desafio 🎯
+
+O desafio é construir esta página e deixá-la o mais parecida possível com o design.
+
+Os usuários devem ser capazes de:
+
+- Ver o layout ideal da interface dependendo do tamanho da tela do dispositivo
+- Ver os estados de foco para todos os elementos interativos da página
+- Escolher quantos caracteres vai ter em sua senha
+- Gerar senhas com letras maiúsculas
+- Gerar senhas com letras minúsculas
+- Geras senhas com Números
+- Gerar senha com simbolos
+- Gerar senhas com todas as quatro opções
+- Copiar a senha gerada
+- Ver o nivel da senha gerada 
+
+## Layout 🎨
+
+É possível visualizar todos os layouts (mobile, tablet e desktop) na pasta da aplicação ` ./design ` .
+
+## Tecnologias e Conceitos Utilizados 💻
+
+- HTML
+- CSS
+- JavaScript
+
+##  Buscando Evolução 🚀
+
+Esse foi um projeto desafiador, as funcionalidades de gerar a senha com diversos tipos de caracteres e mostrar o nível da senha gerada foi um grande desafio.
+
+Apliquei também algo que nunca tinha feito em meus projetos antes, que foi a funcionalidade de copiar texto, foi interessante pesquisar sobre para poder aplicar em meu projeto.
+
+Fiquei muito feliz com o resultado final, consegui desenvolver todas as funcionalidades propostas pelo desafio sem muitos problemas e sem travar em algo ao longo do desenvolvimento.
+
+## Deploy 🔥
+ 
+Para realizar o deploy da minha aplicação, utilizei a própria ferramenta de hospedagem do GitHub, o **GitHub Pages**
+<br>
+<a href="https://gabrielalencs.github.io/Frontend-Mentor-Challenges/News%20Homepage%20Main/">Clique Aqui </a> para visualizar a aplicação.
+
---
+++ Password Generator App/assets/js/scripts.js
@@ -0,0 +1,323 @@
+let password = document.getElementById('password');
+const btnCopyPassword = document.getElementById('copy-password');
+let numberCharacters = document.getElementById('character-length');
+const inputRangeElement = document.getElementById('length-range');
+const buttonsCheckbox = document.querySelectorAll('.input-checkbox');
+const btnUppercaseLetters = document.getElementById('checkbox-uppercase-letters');
+const btnLowercaseLetters = document.getElementById('checkbox-lowercase-letters');
+const btnNumbers = document.getElementById('checkbox-numbers');
+const btnSymbols = document.getElementById('checkbox-symbols');
+const passwordStrenghtText = document.getElementById('password-strenght');
+const elementsPasswordLevel = document.querySelectorAll('.password-level-item');
+const btnPasswordGenerator = document.getElementById('btn-generator');
+
+const buttonsCheckArr = [...buttonsCheckbox];
+const elementStrenghtArr = [...elementsPasswordLevel];
+
+
+
+// functions
+
+const calculatePercentageBar = () => {
+
+    const screenWidth = window.innerWidth;
+
+    const percentage = ( inputRangeElement.value - inputRangeElement.min ) / ( inputRangeElement.max - inputRangeElement.min );
+
+    screenWidth > 480
+        ? inputRangeElement.style.setProperty('--boxAfterWidth', `${percentage * 95}%`)
+        : inputRangeElement.style.setProperty('--boxAfterWidth', `${percentage * 92.5}%`);
+
+};
+
+
+const showBarValue = () => {
+
+    updatePasswordButtonState();
+
+    let barValue = inputRangeElement.value;
+
+    numberCharacters.textContent = barValue;
+
+};
+
+
+(function markButtonsChecked() {
+
+    buttonsCheckbox.forEach(btn => {
+
+        btn.addEventListener('click', () => {
+
+            btn.classList.toggle('checked');
+
+        });
+
+    });
+
+}());
+
+
+const generateUpperCasePassword = (passwordLength) => {
+
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    let generatedPassword = '';
+
+    for ( let index = 0; index < passwordLength; index++ ) {
+
+        let randomIndex = Math.floor(Math.random() * alphabet.length);
+
+        generatedPassword += alphabet[randomIndex];
+
+    }
+
+    return generatedPassword;
+
+};
+
+
+const generateLowerCasePassword = (passwordLength) => {
+
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+    let generatedPassword = '';
+
+    for ( let index = 0; index < passwordLength; index++ ) {
+
+        let randomIndex = Math.floor(Math.random() * alphabet.length);
+
+        generatedPassword += alphabet[randomIndex];
+
+    }
+
+    return generatedPassword;
+
+};
+
+
+const generatePasswordNumbers = (passwordLength) => {
+
+    const numbers = '0123456789';
+
+    let generatedPassword = '';
+
+    for ( let index = 0; index < passwordLength; index++ ) {
+
+        let randomIndex = Math.floor(Math.random() * numbers.length);
+
+        generatedPassword += numbers[randomIndex];
+
+    }
+
+    return generatedPassword;
+
+}
+
+
+const generatePasswordSymbols = (passwordLength) => {
+
+    const symbols = '!@#$%¨&*()_-+=`{[^~<>:;/?';
+
+    let generatedPassword = '';
+
+    for ( let index = 0; index < passwordLength; index++ ) {
+
+        let randomIndex = Math.floor(Math.random() * symbols.length);
+
+        generatedPassword += symbols[randomIndex];
+
+    }
+
+    return generatedPassword;
+
+}
+
+
+const updatePasswordButtonState = () => {
+
+    const algumBotaoMarcado = Array.from(buttonsCheckbox).some(btn => btn.classList.contains('checked'));
+
+    btnPasswordGenerator.disabled = !algumBotaoMarcado;
+
+    buttonsCheckbox.forEach(btn => {
+
+        btn.addEventListener('click', () => {
+
+            if ( inputRangeElement.value > 0 ) {
+
+                updatePasswordButtonState();
+
+            }
+
+        });
+
+    });
+
+}
+updatePasswordButtonState();
+
+
+const generateFinalPassword = () => {
+
+    let passwordAllCharacters = '';
+
+    let finalPasswordGenerated = '';
+
+
+    if ( btnUppercaseLetters.classList.contains('checked') ) {
+        let functionReturn = generateUpperCasePassword(inputRangeElement.value);
+
+        passwordAllCharacters += functionReturn;
+    }
+
+    if ( btnLowercaseLetters.classList.contains('checked') ) {
+        let functionReturn = generateLowerCasePassword(inputRangeElement.value);
+
+        passwordAllCharacters += functionReturn;
+    }
+
+    if ( btnNumbers.classList.contains('checked') ) {
+        let functionReturn = generatePasswordNumbers(inputRangeElement.value);
+
+        passwordAllCharacters += functionReturn;
+    }
+
+
+    if ( btnSymbols.classList.contains('checked') ) {
+        let functionReturn = generatePasswordSymbols(inputRangeElement.value);
+
+        passwordAllCharacters += functionReturn;
+    }
+
+
+    for ( let index = 0; index < inputRangeElement.value; index++ ) {
+        let randomIndex = Math.floor(Math.random() * passwordAllCharacters.length);
+
+        finalPasswordGenerated += passwordAllCharacters[randomIndex];
+    }
+
+
+    password.classList.add('password-generated');
+
+    password.textContent = finalPasswordGenerated;
+
+};
+
+function checksPasswordLevelBasedRange() {
+
+    passwordStrenghtText.classList.remove('hide');
+
+    if ( inputRangeElement.value <= 5 ) {
+
+        elementStrenghtArr[0].style.backgroundColor = '#f64a4a';
+        elementStrenghtArr[0].style.borderColor = '#f64a4a';
+
+        elementStrenghtArr[1].style.backgroundColor = 'transparent';
+        elementStrenghtArr[1].style.borderColor = '#E6E5EA';
+
+        elementStrenghtArr[2].style.backgroundColor = 'transparent';
+        elementStrenghtArr[2].style.borderColor = '#E6E5EA';
+
+        elementStrenghtArr[3].style.backgroundColor = 'transparent';
+        elementStrenghtArr[3].style.borderColor = '#E6E5EA';
+
+
+        passwordStrenghtText.textContent = 'TOO WEAK';
+
+
+    } else if ( inputRangeElement.value <= 10 ) {
+
+        elementStrenghtArr[0].style.backgroundColor = '#fb7c58';
+        elementStrenghtArr[0].style.borderColor = '#fb7c58';
+
+        elementStrenghtArr[1].style.backgroundColor = '#fb7c58';
+        elementStrenghtArr[1].style.borderColor = '#fb7c58';
+
+        elementStrenghtArr[2].style.backgroundColor = 'transparent';
+        elementStrenghtArr[2].style.borderColor = '#E6E5EA';
+
+        elementStrenghtArr[3].style.backgroundColor = 'transparent';
+        elementStrenghtArr[3].style.borderColor = '#E6E5EA';
+
+
+        passwordStrenghtText.textContent = 'WEAK';
+
+
+    } else if ( inputRangeElement.value <= 15 ) {
+
+        elementStrenghtArr[0].style.backgroundColor = '#f8cd65';
+        elementStrenghtArr[0].style.borderColor = '#f8cd65';
+
+        elementStrenghtArr[1].style.backgroundColor = '#f8cd65';
+        elementStrenghtArr[1].style.borderColor = '#f8cd65';
+
+        elementStrenghtArr[2].style.backgroundColor = '#f8cd65';
+        elementStrenghtArr[2].style.borderColor = '#f8cd65';
+
+        elementStrenghtArr[3].style.backgroundColor = 'transparent';
+        elementStrenghtArr[3].style.borderColor = '#E6E5EA';
+
+
+        passwordStrenghtText.textContent = 'MEDIUM';
+
+
+    } else if ( inputRangeElement.value > 15 ) {
+
+        elementStrenghtArr.forEach(element => {
+            element.style.backgroundColor = '#a4ffaf';
+            element.style.borderColor = '#a4ffaf';
+        });
+
+        passwordStrenghtText.textContent = 'STRONG';
+
+    }
+
+}
+
+
+const checkPasswordLevel = () => {
+
+    for ( const iterator of buttonsCheckArr ) {
+
+        if (iterator.classList.contains('checked')) {
+
+            checksPasswordLevelBasedRange();
+        }
+
+    }
+
+};
+
+
+
+// events
+
+inputRangeElement.addEventListener("input", calculatePercentageBar);
+
+inputRangeElement.addEventListener("input", showBarValue);
+
+btnPasswordGenerator.addEventListener('click', generateFinalPassword);
+
+btnPasswordGenerator.addEventListener('click', checkPasswordLevel);
+
+btnCopyPassword.addEventListener('click', () => {
+
+    if ( password.textContent !== 'P4$5W0rD!' ) {
+
+        const textToCopy = password.textContent;
+
+        const tempInput = document.createElement('input');
+
+        tempInput.value = textToCopy;
+
+        document.body.appendChild(tempInput);
+
+        tempInput.select();
+
+        document.execCommand('copy');
+
+        document.body.removeChild(tempInput);
+        
+    }
+
+});
