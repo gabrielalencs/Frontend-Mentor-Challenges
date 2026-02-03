a7dfe53e222e21d9e4b7ef53de1016a89bff3452
Gabriel de Alencar <gabrieldealencar.dev@gmail.com>
2026-02-03 10:16:41 -0300

    developer [21]

--- Weather App/src/App.jsx
+++ Weather App/src/App.jsx
@@ -1,15 +1,19 @@
 import CurrentWeatherCard from './components/CurrentWeatherCard'
 import Header from './components/Header'
+import MetricsRow from './components/MetricsRow'
 import SearchPlace from './components/SearchPlace'
 
 function App() {
     return (
         <>
             <Header />
-            <main>
+            <main className='mb-72'>
                 <SearchPlace />
-                <div className='max-w-304 mx-auto px-6 mt-12 md:px-10'>
-                    <CurrentWeatherCard />
+                <div className='max-w-304 mx-auto px-6 mt-16 md:px-10'>
+                    <div className='flex flex-col gap-7'>
+                        <CurrentWeatherCard />
+                        <MetricsRow />
+                    </div>
                 </div>
             </main>
         </>
--- Weather App/src/components/SearchPlace/index.jsx
+++ Weather App/src/components/SearchPlace/index.jsx
@@ -5,7 +5,7 @@ const SearchPlace = () => {
         <section className='mt-8'>
             <h1 className='font-["Bricolage_Grotesque",sans-serif] text-white font-bold text-center leading-14 text-5xl lg:text-6xl'>How's the <br className='sm:hidden' /> sky looking <br className='md:hidden' /> today?</h1>
 
-            <div className='mt-14 px-5 w-full flex flex-col sm:flex-row items-center justify-center gap-2 max-w-300 mx-auto'>
+            <div className='mt-11 px-5 w-full flex flex-col sm:flex-row items-center justify-center gap-2 max-w-300 mx-auto'>
                 <div className='relative cursor-pointer w-full sm:w-1/2 flex items-center gap-2 py-3 px-5 bg-[#272541] hover:bg-[#312f4b] rounded-lg'>
                     <img src={SearchIcon} alt="Icon Search" />
                     <input
